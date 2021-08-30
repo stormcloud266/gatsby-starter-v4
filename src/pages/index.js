23ce6d7f2cbd8d8eae46df31066d545749580364
Tawnee <allprt89@gmail.com>
2021-08-30 14:17:48 -0600

    clean up index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,156 +1,23 @@
 import React from 'react'
-import { Link, graphql } from 'gatsby'
-
 import Layout from '@global/layout/layout'
 import Seo from '@global/seo'
-import { Container, Flex, Card, Title, Grid, Button } from '@UI'
+import { Container, Title } from '@UI'
 
-const IndexPage = ({ data }) => (
+const IndexPage = () => (
 	<Layout>
 		<Seo />
-		<Container wrapperSm section textBlock>
-			<h1>Hello</h1>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore,
-				deserunt facere enim ullam quos expedita repellat inventore porro quam
-				dolorum.
+		<Container section wrapperSm>
+			<p
+				style={{
+					textAlign: 'center',
+					fontSize: '2rem',
+				}}
+			>
+				Welcome to{' '}
 			</p>
-			<p>
-				Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore,
-				deserunt facere enim ullam quos expedita repellat inventore porro quam
-				dolorum.
-			</p>
-			<Button to='/examples' cta>
-				Examples Page
-			</Button>
-		</Container>
-
-		<Container bgAccent>
-			<Container wrapper section textBlock>
-				<Flex autoLayout>
-					<div>
-						<h1>Hello</h1>
-						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore,
-							deserunt facere enim ullam quos expedita repellat inventore porro
-							quam dolorum.
-						</p>
-						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore,
-							deserunt facere enim ullam quos expedita repellat inventore porro
-							quam dolorum.
-						</p>
-						<Button onClick={() => console.log('hi')}>Examples Page</Button>
-					</div>
-					<div>
-						<h1>Hello</h1>
-						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore,
-							deserunt facere enim ullam quos expedita repellat inventore porro
-							quam dolorum.
-						</p>
-						<p>
-							Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore,
-							deserunt facere enim ullam quos expedita repellat inventore porro
-							quam dolorum.
-						</p>
-						<Button href='https://music.com'>Examples Page</Button>
-					</div>
-				</Flex>
-			</Container>
-		</Container>
-
-		<Container wrapper section>
-			<Title>hello</Title>
-			<Flex autoLayout>
-				<Card image={data.cardImage} shadow>
-					<div
-						style={{
-							display: 'flex',
-							flexDirection: 'column',
-							justifyContent: 'space-between',
-						}}
-					>
-						<div>
-							<h3>Lorem ipsum dolor sit amet.</h3>
-							<p>
-								Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet
-								consectetur adipisicing elit. Quam laboriosam exercitationem
-								quisquam.
-							</p>
-						</div>
-						<Button href='https://google.com'>click</Button>
-					</div>
-				</Card>
-				<Card image={data.cardImage} outline>
-					<div
-						style={{
-							display: 'flex',
-							flex: 1,
-							flexDirection: 'column',
-							justifyContent: 'space-between',
-						}}
-					>
-						<div>
-							<h3>Lorem ipsum dolor sit amet.</h3>
-							<p>Lorem ipsum</p>
-						</div>
-						<a href='/' className='button'>
-							click
-						</a>
-					</div>
-				</Card>
-				<Card image={data.cardImage} outline>
-					<div
-						style={{
-							display: 'flex',
-							flex: 1,
-							flexDirection: 'column',
-							justifyContent: 'space-between',
-						}}
-					>
-						<div>
-							<h3>Lorem ipsum dolor sit amet.</h3>
-							<p>Lorem ipsum</p>
-						</div>
-						<a href='/' className='button'>
-							click
-						</a>
-					</div>
-				</Card>
-			</Flex>
-		</Container>
-
-		<Container bgAccent>
-			<Container section wrapper>
-				<Grid columns={5} gap='lg'>
-					{['1', '2', '3', '4', '1', '2', '3', '4', '1', '2', '3', '4'].map(
-						(item) => (
-							<div
-								style={{
-									width: '100%',
-									height: 60,
-									background: 'pink',
-								}}
-							>
-								{item}
-							</div>
-						)
-					)}
-				</Grid>
-			</Container>
+			<Title tag='h1'>Gatsby Starter Stormcloud</Title>
 		</Container>
 	</Layout>
 )
 
 export default IndexPage
-
-export const query = graphql`
-	query HomePageQuery {
-		cardImage: file(relativePath: { eq: "meta-card-banner.png" }) {
-			childImageSharp {
-				gatsbyImageData(layout: CONSTRAINED, width: 400, placeholder: BLURRED)
-			}
-		}
-	}
-`
