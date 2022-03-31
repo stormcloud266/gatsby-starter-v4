4dd8aa267de84a3761efd00f5589a6edc2de0d3a
Tawnee <allprt89@gmail.com>
2022-03-31 15:56:27 -0600

    change back pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,18 +1,17 @@
 import React from 'react'
-// import { Layout, Seo } from '@global'
-// import { Container, Button, Title } from '@UI'
+import { Layout, Seo } from '@global'
+import { Container, Button, Title } from '@UI'
 
 const NotFoundPage = () => {
 	return (
-		<div></div>
-		// <Layout>
-		// 	<Seo title='Not Found' />
-		// 	<Container section wrapperSm textBlock textCenter>
-		// 		<Title>Page Not Found</Title>
-		// 		<p>The page you were looking for does not exist.</p>
-		// 		<Button to='/'>Return Home</Button>
-		// 	</Container>
-		// </Layout>
+		<Layout>
+			<Seo title='Not Found' />
+			<Container section wrapperSm textBlock textCenter>
+				<Title>Page Not Found</Title>
+				<p>The page you were looking for does not exist.</p>
+				<Button to='/'>Return Home</Button>
+			</Container>
+		</Layout>
 	)
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,66 +1,68 @@
 import React from 'react'
-// import { graphql } from 'gatsby'
-// import { Layout, Seo } from '@global'
-// import { Container, Title, BackgroundImage } from '@UI'
-// import { Slide, Fade, Zoom } from '@animations'
+import { graphql } from 'gatsby'
+import { Layout, Seo } from '@global'
+import { Container, Title, BackgroundImage } from '@UI'
+import { Slide, Fade, Zoom } from '@animations'
 
-const IndexPage = () => {
+const IndexPage = ({ data }) => {
 	return (
-		<div></div>
-		// <Layout>
-		//  <Seo />
+		<Layout>
+			<Seo />
 
-		// 	<div
-		// 		style={{
-		// 			minHeight: '100vh',
-		// 			position: 'relative',
-		// 			display: 'flex',
-		// 			alignItems: 'center',
-		// 			justifyContent: 'center',
-		// 			overflow: 'hidden',
-		// 		}}
-		// 	>
-		// 		<Zoom>
-		// 			<BackgroundImage image={data.file} overlayColor='rgba(100,4,60, .4)' />
-		// 		</Zoom>
-		// 		<Title style={{ position: 'relative', zIndex: 3 }}>Hello</Title>
-		// 	</div>
+			<div
+				style={{
+					minHeight: '100vh',
+					position: 'relative',
+					display: 'flex',
+					alignItems: 'center',
+					justifyContent: 'center',
+					overflow: 'hidden',
+				}}
+			>
+				<Zoom>
+					<BackgroundImage
+						image={data.file}
+						overlayColor='rgba(100,4,60, .4)'
+					/>
+				</Zoom>
+				<Title style={{ position: 'relative', zIndex: 3 }}>Hello</Title>
+			</div>
 
-		// 	<Container
-		// 		section
-		// 		wrapper
-		// 		textCenter
-		// 		style={{
-		// 			minHeight: '82vh',
-		// 			display: 'flex',
-		// 			alignItems: 'center',
-		// 			justifyContent: 'center',
-		// 		}}
-		// 	>
-		// 		<Fade x={-40} threshold={0.8} noOpacityAnim transformDuration={1.7}>
-		// 			<Slide
-		// 				from='left'
-		// 				style={{
-		// 					margin: '0 auto',
-		// 					padding: '8px',
-		// 				}}
-		// 			>
-		// 				<Title tag='h1'>Gatsby Starter Stormcloud</Title>
-		// 			</Slide>
-		// 		</Fade>
-		// 	</Container>
-		// </Layout>
+			<Container
+				section
+				wrapper
+				textCenter
+				style={{
+					minHeight: '82vh',
+					display: 'flex',
+					alignItems: 'center',
+					justifyContent: 'center',
+				}}
+			>
+				<Fade x={-40} threshold={0.8} noOpacityAnim transformDuration={1.7}>
+					<Slide
+						from='left'
+						style={{
+							margin: '0 auto',
+							padding: '8px',
+						}}
+					>
+						<Title tag='h1'>Gatsby Starter Stormcloud</Title>
+					</Slide>
+				</Fade>
+			</Container>
+		</Layout>
 	)
 }
 
 export default IndexPage
 
-// export const query = graphql`
-// 	query HomePageQuery {
-// 		file(name: { eq: "hero" }) {
-// 			childImageSharp {
-// 				gatsbyImageData(layout: FULL_WIDTH, placeholder: BLURRED)
-// 			}
-// 		}
-// 	}
-// `
+export const query = graphql`
+	query HomePageQuery {
+		file(name: { eq: "hero" }) {
+			childImageSharp {
+				gatsbyImageData(layout: FULL_WIDTH, placeholder: BLURRED)
+			}
+		}
+	}
+`
