70517556fc753ea625bf887b4e4ed7ab145639d8
Tawnee <allprt89@gmail.com>
2022-03-31 11:26:22 -0600

    add things

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,18 @@
 import React from 'react'
-import { Layout, Seo } from '@global'
-import { Container, Button, Title } from '@UI'
+// import { Layout, Seo } from '@global'
+// import { Container, Button, Title } from '@UI'
 
 const NotFoundPage = () => {
 	return (
-		<Layout>
-			<Seo title='Not Found' />
-			<Container section wrapperSm textBlock textCenter>
-				<Title>Page Not Found</Title>
-				<p>The page you were looking for does not exist.</p>
-				<Button to='/'>Return Home</Button>
-			</Container>
-		</Layout>
+		<div></div>
+		// <Layout>
+		// 	<Seo title='Not Found' />
+		// 	<Container section wrapperSm textBlock textCenter>
+		// 		<Title>Page Not Found</Title>
+		// 		<p>The page you were looking for does not exist.</p>
+		// 		<Button to='/'>Return Home</Button>
+		// 	</Container>
+		// </Layout>
 	)
 }
 
